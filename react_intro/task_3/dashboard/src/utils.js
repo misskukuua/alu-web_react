d192f468dbcf3a6c7e48dadb21d0261f95c430bb
misskukuua <p.asare@alustudent.com>
2024-05-17 01:02:41 +0000

    Create basic tests with four tests

--- react_intro/task_3/dashboard/src/utils.js
+++ react_intro/task_3/dashboard/src/utils.js
@@ -1,16 +1,20 @@
+
 export function getFullYear() {
-    return new Date().getFullYear();
+  return new Date().getFullYear();
+}
+
+export function getFooterCopy(isIndex) {
+  let msg;
+
+  if (isIndex) {
+    msg = "Holberton School";
+  } else {
+    msg = "Holberton School main dashboard";
   }
-  
-  export function getFooterCopy(isIndex) {
-    let msg;
-  
-    if (isIndex) {
-      msg = "Holberton School";
-    } else {
-      msg = "Holberton School main dashboard";
-    }
-  
-    return msg;
-  }
-  
+
+  return msg;
+}
+
+export function getLatestNotification() {
+  return "<strong>Urgent requirement</strong> - complete by EOD";
+}
