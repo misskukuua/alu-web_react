20aa17ecee55d06baf40ab7821fa05464f56b6b2
misskukuua <p.asare@alustudent.com>
2024-05-17 01:00:28 +0000

    Modify the App

---
+++ react_intro/task_2/dashboard/src/utils.js
@@ -0,0 +1,16 @@
+export function getFullYear() {
+    return new Date().getFullYear();
+  }
+  
+  export function getFooterCopy(isIndex) {
+    let msg;
+  
+    if (isIndex) {
+      msg = "Holberton School";
+    } else {
+      msg = "Holberton School main dashboard";
+    }
+  
+    return msg;
+  }
+  
