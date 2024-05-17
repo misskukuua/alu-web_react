5f9a2c7fe951cb692dab815e6fb744370c16ffd3
misskukuua <p.asare@alustudent.com>
2024-05-17 00:14:25 +0000

    Basic application

---
+++ react_intro/task_0/dashboard/src/App.js
@@ -0,0 +1,22 @@
+import React from "react";
+import "./App.css";
+import logo from "./logo.png";
+
+function App() {
+  return (
+    <>
+      <div className="App-header">
+        <img src={logo}  alt="logo" />
+        <h1>School dashboard</h1>
+      </div>
+      <div className="App-body">
+        <p>Login to access the full dashboard</p>
+      </div>
+      <div className="App-footer">
+        <p>Copyright 2020 - holberton School</p>
+      </div>
+    </>
+  );
+}
+
+export default App;
