c860b9fc6d9ab3c5765bfb3f65ce1bb90246bbcc
misskukuua <p.asare@alustudent.com>
2024-05-21 23:26:56 +0000

    utils

--- React_component/task_0/dashboard/src/utils/utils.js
+++ React_component/task_0/dashboard/src/utils/utils.js
@@ -1,5 +1,18 @@
-export const getFullYear = () => new Date().getFullYear();
 
-export const getFooterCopy = (isIndex) => (isIndex ? "Holberton School" : "Holberton School main dashboard");
+// Return the current year
+export function getFullYear() {
+  return new Date().getFullYear();
+}
 
-export const getLatestNotification = () => "<strong>Urgent Requirement</strong> - complete by EOD";
+export function getFooterCopy(isIndex) {
+  if (isIndex) {
+      return 'Holberton School.';
+  }
+  else {
+      return 'Holberton School main dashboard';
+  }
+}
+
+export function getLatestNotification() {
+  return {__html: '<strong>Urgent requirement</strong> - complete by EOD'};
+}
