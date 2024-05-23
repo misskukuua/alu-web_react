5fd8e73283bf44f31de96f095174975d18078968
misskukuua <p.asare@alustudent.com>
2024-05-23 09:58:21 +0000

    Add a test

--- React_component/task_5/dashboard/src/Notifications/Notifications.test.js
+++ React_component/task_5/dashboard/src/Notifications/Notifications.test.js
@@ -1,19 +1,41 @@
 // Notifications.test.js
-import React from "react";
-import { shallow } from "enzyme";
-import Notifications from "./Notifications";
+import React from 'react';
+import { shallow } from 'enzyme';
+import Notifications from './Notifications';
+import NotificationItem from './NotificationItem';
 
-describe("Notifications Component", () => {
-  it("should log the message when calling markAsRead function", () => {
-    const spy = jest.spyOn(console, "log");
+describe('<Notifications />', () => {
+  it('does not rerender when updating the props with the same list', () => {
     const listNotifications = [
-      { id: 1, type: "default", value: "Notification 1" },
-      { id: 2, type: "urgent", value: "Notification 2" },
+      { id: 1, type: 'default', value: 'New course available' },
+      { id: 2, type: 'urgent', value: 'New resume available' }
     ];
+
     const wrapper = shallow(<Notifications listNotifications={listNotifications} />);
-    const instance = wrapper.instance();
-    instance.markAsRead(1);
-    expect(spy).toHaveBeenCalledWith("Notification 1 has been marked as read");
-    spy.mockRestore();
+    
+    const shouldComponentUpdateSpy = jest.spyOn(Notifications.prototype, 'shouldComponentUpdate');
+    wrapper.setProps({ listNotifications });
+    expect(shouldComponentUpdateSpy).toHaveReturnedWith(false);
+
+    shouldComponentUpdateSpy.mockRestore();
+  });
+
+  it('rerenders when updating the props with a longer list', () => {
+    const initialListNotifications = [
+      { id: 1, type: 'default', value: 'New course available' }
+    ];
+
+    const newListNotifications = [
+      { id: 1, type: 'default', value: 'New course available' },
+      { id: 2, type: 'urgent', value: 'New resume available' }
+    ];
+
+    const wrapper = shallow(<Notifications listNotifications={initialListNotifications} />);
+    
+    const shouldComponentUpdateSpy = jest.spyOn(Notifications.prototype, 'shouldComponentUpdate');
+    wrapper.setProps({ listNotifications: newListNotifications });
+    expect(shouldComponentUpdateSpy).toHaveReturnedWith(true);
+
+    shouldComponentUpdateSpy.mockRestore();
   });
 });
