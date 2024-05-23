2ef0a660f4f7324dd7767dbe5b7196e230dbada4
misskukuua <p.asare@alustudent.com>
2024-05-23 14:57:43 +0000

    logins.js

--- React_inline_styling/task_1/dashboard/src/Login/Login.test.js
+++ React_inline_styling/task_1/dashboard/src/Login/Login.test.js
@@ -1,8 +1,16 @@
 import { shallow } from "enzyme";
 import React from "react";
 import Login from "./Login";
+import { StyleSheetTestUtils } from "aphrodite";
 
 describe("<Login />", () => {
+  beforeAll(() => {
+    StyleSheetTestUtils.suppressStyleInjection();
+  });
+  afterAll(() => {
+    StyleSheetTestUtils.clearBufferAndResumeStyleInjection();
+  });
+
   it("Login renders without crashing", () => {
     const wrapper = shallow(<Login />);
     expect(wrapper.exists()).toEqual(true);
@@ -10,11 +18,11 @@ describe("<Login />", () => {
   it("Verify that the components render 2 input", () => {
     const wrapper = shallow(<Login />);
     wrapper.update();
-    expect(wrapper.find("div.login input")).toHaveLength(2);
+    expect(wrapper.find("div input")).toHaveLength(2);
   });
   it("Verify that the components render 2 label", () => {
     const wrapper = shallow(<Login />);
     wrapper.update();
-    expect(wrapper.find("div.login label")).toHaveLength(2);
+    expect(wrapper.find("div label")).toHaveLength(2);
   });
 });
