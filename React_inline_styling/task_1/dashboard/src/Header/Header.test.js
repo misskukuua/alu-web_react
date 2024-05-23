a89aa31c3f0d056ee468a903c8f5f9567c2811c4
misskukuua <p.asare@alustudent.com>
2024-05-23 14:55:47 +0000

    hearder.js

--- React_inline_styling/task_1/dashboard/src/Header/Header.test.js
+++ React_inline_styling/task_1/dashboard/src/Header/Header.test.js
@@ -1,8 +1,16 @@
 import { shallow } from "enzyme";
 import React from "react";
 import Header from "./Header";
+import { StyleSheetTestUtils } from "aphrodite";
 
 describe("<Header />", () => {
+  beforeAll(() => {
+    StyleSheetTestUtils.suppressStyleInjection();
+  });
+  afterAll(() => {
+    StyleSheetTestUtils.clearBufferAndResumeStyleInjection();
+  });
+
   it("Header renders without crashing", () => {
     const wrapper = shallow(<Header />);
     expect(wrapper.exists()).toEqual(true);
@@ -10,11 +18,11 @@ describe("<Header />", () => {
   it("Verify that the components render img", () => {
     const wrapper = shallow(<Header />);
     wrapper.update();
-    expect(wrapper.find("div.header img")).toHaveLength(1);
+    expect(wrapper.find("div img")).toHaveLength(1);
   });
   it("Verify that the components render h1", () => {
     const wrapper = shallow(<Header />);
     wrapper.update();
-    expect(wrapper.find("div.header h1")).toHaveLength(1);
+    expect(wrapper.find("div h1")).toHaveLength(1);
   });
 });
